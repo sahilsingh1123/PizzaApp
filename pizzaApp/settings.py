f34add2508ebac9dfa8492c93c2cda4c14812312
sahil singh <sahils@splunk.com>
2021-05-30 20:25:41 +0530

    moved from postgres to sqllite db

--- pizzaApp/settings.py
+++ pizzaApp/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'pizzaApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'pizzacorp',
+#         'PASSWORD': 'pizzaCorp',
+#         'HOST': 'localhost'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'pizzacorp',
-        'PASSWORD': 'pizzaCorp',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
